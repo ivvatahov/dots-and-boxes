6ea83b586fdee3b850fab2abb8e8c4acc38d8444
Ilia Vatahov <ivvatahov@gmail.com>
2014-08-29 18:44:11 +0300

    adding test for Vector2f class

--- spec/core/vector2f.rb
+++ spec/core/vector2f.rb
@@ -9,59 +9,59 @@ describe "Vector2f" do
     vector.x
     vector.y
   end
-  
+
   it "has the same coordinates after initialization" do
     vector.x = 3
     vector.y = 5
   end
 
-  it "can return its own length" do 
-    expect(5.830951894845301).to vector.length
+  it "can return its own length" do
+    expect(vector.length).to be 5.830951894845301 
   end
 
   context "operations with vectors" do
     it "maintains scalar multiplication with another vector" do
       other = make_vector 7, 9
       result = vector * other
-      expect(66).to result
+      expect(result).to be 66
     end
 
-    it "maintains multiplication with numeric" do 
+    it "maintains multiplication with numeric" do
       result = vector * 4
-      expect(12).toresult.x
-      expect(20).toresult.y
+      expect(result.x).to be 12
+      expect(result.y).to be 20
     end
 
     it "maintains addition with another vector" do
       other = make_vector 4, 77
       result = vector + other
-      expect(7).to result.x
-      expect(82).toresult.y
+      expect(result.x).to be 7
+      expect(result.y).to be 82
     end
 
     it "maintains substraction with another vector" do
       other = make_vector 54, 5
       result = vector - other
-      expect(51).to result.x
-      expect(0).toresult.y
+      expect(result.x).to be -51
+      expect(result.y).to be 0
     end
 
     it "returns new vector after addition" do
       other = make_vector 11, 0
       result = vector + other
-      expect(false).to result.equal?(vector)
-      expect(false).to result.equal?(other) 
+      expect(result.equal? vector).to be false
+      expect(result.equal? other).to be false
     end
 
-    it "returns new vector after substraction" do 
+    it "returns new vector after substraction" do
       other = make_vector -5, 6
       result = vector - other
-      expect(false).to result.equal?(vector)
-      expect(false ).to result.equal?(other)
+      expect(result.equal? vector).to be false
+      expect(result.equal? other).to be false
     end
   end
-end
 
-def make_vector(*args)
-  Core::GameMath::Vector2f.new *args
+  def make_vector(*args)
+    Core::GameMath::Vector2f.new *args
+  end
 end
